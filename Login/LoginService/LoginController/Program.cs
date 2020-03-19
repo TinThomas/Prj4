ee563492280890c9c3f1b5c392b80ae0b7384c7c
Jonas-au18 <Jonas.sdu@gmail.com>
2020-03-19 09:19:28 +0100

    Added sub solutions

---
+++ Login/LoginService/LoginController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoginController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Login/LoginService/SignupController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SignupController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
