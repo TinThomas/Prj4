b6e04d28decfeef0c4dcda4878f1c834796ffd48
Thomas <thomas.baerentsen@gmail.com>
2020-03-19 11:19:04 +0100

    Added UserN database with EFCore

---
+++ Login/LoginService/SignupController/UserNModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace SignupController
+{
+    public class UserNContext : DbContext
+    {
+        public DbSet<UserN> UserNs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlite("Data Source=UserNDB.db");
+    }
+
+    public class UserN
+    {
+        public int UserNId { get; set; }
+        public string Email { get; set; }
+        public string userName { get; set; }
+    }
+}
