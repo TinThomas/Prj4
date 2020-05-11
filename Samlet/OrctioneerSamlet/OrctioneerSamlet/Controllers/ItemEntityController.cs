549b5d6c136afa23eff8488cf4c8f6f386efd370
Sebastian <sebastian@nielsen2u.dk>
2020-05-11 17:42:40 +0200

    bid entites something

--- Samlet/OrctioneerSamlet/OrctioneerSamlet/Controllers/ItemEntityController.cs
+++ Samlet/OrctioneerSamlet/OrctioneerSamlet/Controllers/ItemEntityController.cs
@@ -46,7 +46,57 @@ namespace VareDatabase.Controllers
             json = JsonConvert.SerializeObject(_dbLogic.GetAll(), Formatting.Indented);
             return json;
         }
+        //populær
+        //[HttpGet]
+        //[Route("Home/item/pop")]
+        //public ActionResult<string> GetPopularItems()
+        //{
+        //    var items = _dbLogic.GetAll();
+        //    List<ItemEntity> popularItems = new List<ItemEntity>();
+        //    foreach(var item in items)
+        //    {
+        //        if(popularItems.Count == 0 && item.Bids.Count > 0)
+        //        {
+        //            popularItems.Add(item);
+        //        }
+        //        else if(item.Bids.Count > popularItems.Last().Bids.Count && item.Bids.Count > 0)
+        //        {
+        //            //iterate down to find right index to insert
+        //            for(int i = 0; i < popularItems.Count; i++)
+        //            {
+        //                if(popularItems[i].Bids.Count < item.Bids.Count)
+        //                {
+        //                    var temp = popularItems[i];
+        //                    popularItems[i] = item;
+        //                    popularItems[i] = temp;
 
+        //                }
+        //            }
+        //        }
+        //        else if(popularItems.Count < 10)
+        //        {
+        //            popularItems.Add(item);
+        //        }
+        //    }
+        //    json = JsonConvert.SerializeObject(popularItems, Formatting.Indented);
+        //    return json;
+        //}
+        //newest
+        [HttpGet]
+        [Route("Home/item/new")]
+        public ActionResult<string> GetNewestItems()
+        {
+            json = JsonConvert.SerializeObject(_dbLogic.GetAll(), Formatting.Indented);
+            return json;
+        }
+        //about to expire
+        [HttpGet]
+        [Route("Home/item/expire")]
+        public ActionResult<string> GetExpiringItems()
+        {
+            json = JsonConvert.SerializeObject(_dbLogic.GetAll(), Formatting.Indented);
+            return json;
+        }
         [HttpGet]
         [Route("Home/item/tag/{search?}")]
         public ActionResult<string> GetTag(string search)
@@ -92,16 +142,18 @@ namespace VareDatabase.Controllers
                 {
                     await file.CopyToAsync(fileStream);
                 }
-
-                //using (var stream = System.IO.File.Create(path))
-                //{
-                //    await file.CopyToAsync(stream);
-                //}
                 string newFileName = Guid.NewGuid().ToString();
-                //rename file
                 System.IO.File.Move(path, Path.Combine(imgFolder,newFileName));
             }
             return Ok(file.Length);
+        }
+
+        public async Task<ActionResult<string>> LoadPicture(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+
+            return sr.ReadLine();
+
         }
     }
 }
--- Samlet/OrctioneerSamlet/OrctioneerSamlet/DatabaseLogic.cs
+++ Samlet/OrctioneerSamlet/OrctioneerSamlet/DatabaseLogic.cs
@@ -6,37 +6,43 @@ using VareDatabase.Interfaces;
 using VareDatabase.Repo;
 using VareDatabase.Repo.Auction;
 using VareDatabase.Models;
+using VareDatabase.Interfaces.Auction;
 
 namespace VareDatabase
 {
     public class DatabaseLogic
     {
         private readonly IUnitOfWork unit;
-        private readonly IItemRepository repo;
-        //private ILoginRepository _loginRepo; //does not exist atm
-        public DatabaseLogic(IUnitOfWork unit, IItemRepository repo)
+        private readonly IItemRepository itemRepo;
+        private readonly IBidRepository bidRepo;
+        public DatabaseLogic(IUnitOfWork unit, IItemRepository itemRepo = null, IBidRepository bidRepo = null)
         {
             this.unit = unit;
-            this.repo = repo;
+            this.itemRepo = itemRepo;
+            this.bidRepo = bidRepo;
+        }
+        public void AddBid(BidEntity bid)
+        {
+
         }
         public void AddItem(ItemEntity item)
         {
-            repo.Create(item);
-            //repo.GenerateTags(item);
+            itemRepo.Create(item);
+            //itemRepo.GenerateTags(item);
             unit.Commit();
         }
         public void Delete(ItemEntity item)
         {
-            repo.Delete(item);
+            itemRepo.Delete(item);
         }
 
         public IEnumerable<ItemEntity> GetAll()
         {
-            return repo.GetAll();
+            return itemRepo.GetAll();
         }
         public ItemEntity GetSingle(int id)
         {
-            return repo.Read(id);
+            return itemRepo.Read(id);
         }
         public int Save()
         {
@@ -44,11 +50,11 @@ namespace VareDatabase
         }
         public void AddTag(int id, string newTag)
         {
-            repo.AddTag(id, newTag);
+            itemRepo.AddTag(id, newTag);
         }
         public IEnumerable<ItemEntity> Search(string searchingstring)
         {
-            return repo.Search(searchingstring);
+            return itemRepo.Search(searchingstring);
         }
     }
 }
