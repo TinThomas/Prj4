8a84aaf581fdffb8faabf59782fe710051af531f
Solguden <55831381+Solguden@users.noreply.github.com>
2020-05-12 19:51:17 +0200

    no more moment

--- Samlet/OrctioneerSamlet/OrctioneerSamlet/clientapp/src/components/auction/Auction2.vue
+++ Samlet/OrctioneerSamlet/OrctioneerSamlet/clientapp/src/components/auction/Auction2.vue
@@ -5,7 +5,7 @@
                 <img :src="auc.url">
                 <h5>{{ auc.Title }}</h5>
                 <h5>Buy Out Price: {{ auc.BuyOutPrice }}</h5>
-                <h5>Ends: {{ auc.ExpirationDate | | moment("from", now) }}</h5>
+                <h5>Ends: {{ auc.ExpirationDate | moment("from", now) }}</h5>
 
                 <button @click="navigateToAuction(auc.ItemId)" class="btn btn-secondary">See full auction</button>
 
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-    import moment from 'vue-moment'
+    //import moment from 'vue-moment'
 
     export default{
         props:{
